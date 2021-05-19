a14b56cb5286e06a66c874ddd3b7fdd574cbc0da
Amanda <amandamaarman19911@gmail.com>
2021-05-19 23:09:23 +0200

    added html and css on the web page

---
+++ factory/regNumbers.js
@@ -0,0 +1,25 @@
+function regNum(){
+    var regNumbers = [];
+
+    function setReg(enterRegNum){
+        regNumbers.push(enterRegNum)
+    }
+
+    function getReg(){
+        return theName;
+    }
+
+    function carRg(registrationNumbers){
+        var displayReg = [];
+
+        if(registrationNumbers === 'cape town'){
+            return 'Cape Town'
+        }
+    }
+
+
+    return{
+        setReg,
+        getReg
+    }
+}
